3930674e799115e49b9dd441776c53d95fc79069
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-07-09 11:57:05 +0200

    Failing test for years which are fivisble by 100 but not by 400

--- spec/leapYearsSpec.js
+++ spec/leapYearsSpec.js
@@ -17,4 +17,16 @@ describe("leapYear", function () {
   it("returns true if given year 400", function () {
     expect(leapYear(400)).toBe(true);
   });
+
+  it("returns true if given the year 800", function () {
+    expect(leapYear(800)).toBe(true);
+  });
+
+  it("returns true if given a year which is divisble by 4 but not by 100", function () {
+    expect(leapYear(2004)).toBe(true);
+  });
+
+  it("returns false if given a year which is divisible by 100 but not by 400", function () {
+    expect(leapYear(1700)).toBe(false);
+  });
 });
