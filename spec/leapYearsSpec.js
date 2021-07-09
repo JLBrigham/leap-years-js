d8152c16ecb2dd57c8abe089ba77205cd49b54e5
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-07-09 11:30:10 +0200

    Failing first test for returning true if given year 400

--- spec/leapYearsSpec.js
+++ spec/leapYearsSpec.js
@@ -1,24 +1,20 @@
 // spec/fizzBuzzSpec.js
 
-describe('FizzBuzz', () => {
+// describe('FizzBuzz', () => {
 
-    let fizzBuzz;
-  
-    beforeEach(() => {
-      fizzBuzz = new FizzBuzz();
-    });
-  
-    describe('multiples of 3', () => {
-      it('fizzes for 3', () => {
-        expect(fizzBuzz.play(3)).toEqual('Fizz');
-      });
+//     let fizzBuzz;
 
-      describe('Leapyears', () = {
+//     beforeEach(() => {
+//       fizzBuzz = new FizzBuzz();
+//     });
 
-        let leapYears;
-        
-        beforeEach(() => {
-            leapYears = new LeapYears();
-        })
+//     describe('multiples of 3', () => {
+//       it('fizzes for 3', () => {
+//         expect(fizzBuzz.play(3)).toEqual('Fizz');
+//       });
 
-      })
+describe("leapYear", function () {
+  it("returns true if given year 400", function () {
+    expect(leapYear(400)).tobe(true);
+  });
+});
